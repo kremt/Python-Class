46f8e200e60882ef069cc019d982533cccb2cdc1
kremt <kpcorp02@gmail.com>
2025-08-01 12:50:24 -0400

    problem with initiators
    superclass being passed extra arguments when only one being entered is "self", not sure whats wrong.

--- employee_class.py
+++ employee_class.py
@@ -2,18 +2,18 @@
 class Employee:
     def __init__(self):
         #Get name and number from user for attributes.
-        self.__name = str(input("Please enter the employee's name:"))
-        self.__number = int(input("Please enter the employee's number:"))
+        self.__name = set_name(self)
+        self.__number = set_number(self)
     
     #Setters and Getters for attributes.
-    def set_name(self, name):
-        self.__name = name
+    def set_name(self):
+        self.__name = str(input("Please enter the employee's name:"))
 
     def get_name(self):
         return self.__name
 
-    def set_number(self, number):
-        self.__number = int(number)
+    def set_number(self):
+        self.__number = int(input("Please enter the employee's number:"))
 
     def get_number(self):
         return self.__number
