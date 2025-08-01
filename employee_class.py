c76a585fb267d274a8921c55fb778dac707e30cd
kremt <kpcorp02@gmail.com>
2025-08-01 13:31:20 -0400

    working
    was calling the functions wrong. Did not need to pass self manually.

--- employee_class.py
+++ employee_class.py
@@ -2,18 +2,18 @@
 class Employee:
     def __init__(self):
         #Get name and number from user for attributes.
-        self.__name = set_name(self)
-        self.__number = set_number(self)
+        self.__name = set_name()
+        self.__number = set_number()
     
     #Setters and Getters for attributes.
     def set_name(self):
-        self.__name = str(input("Please enter the employee's name:"))
+        self.__name = input("Please enter the employee's name:")
 
     def get_name(self):
         return self.__name
 
     def set_number(self):
-        self.__number = int(input("Please enter the employee's number:"))
+        self.__number = input("Please enter the employee's number:")
 
     def get_number(self):
         return self.__number
