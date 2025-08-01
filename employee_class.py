90e6c7612416c8bddab0d3977a170f561fb52262
kremt <kpcorp02@gmail.com>
2025-08-01 12:10:58 -0400

    Testing class

---
+++ employee_class.py
@@ -0,0 +1,20 @@
+#Module file for employee class
+class Employee:
+    self.__init__(self)
+    #Get input for attributes name and number.
+    self.__name = str(input("Please enter the employee's name:"))
+    self.__number = int(input("Please enter the employee's number:"))
+    
+    #Setters and Getters for attributes.
+    def set_name(name):
+        self.__name = name
+
+    def get_name():
+        return self.__name
+
+    def set_number(number):
+        self.__number = int(number)
+
+    def get_number():
+        return self.__number
+        
